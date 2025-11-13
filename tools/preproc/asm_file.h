196342eee4621ac33bccd682e51fa668567e7ac2
shachar700 <shachar700@gmail.com>
2025-11-13 20:30:36 +0200

    braille hebrew

--- tools/preproc/asm_file.h
+++ tools/preproc/asm_file.h
@@ -46,6 +46,7 @@ public:
     std::string GetGlobalLabel();
     std::string ReadPath();
     int ReadString(unsigned char* s);
+    char32_t DecodeUtf8Codepoint(size_t &pos);
     int ReadBraille(unsigned char* s);
     bool IsAtEnd();
     void OutputLine();

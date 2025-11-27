4d6735f7764a25ad4159e60cb3ba23c713986f91
shachar700 <shachar700@gmail.com>
2025-11-27 17:47:16 +0200

    translated lotad and seedot house

--- include/config.h
+++ include/config.h
@@ -37,10 +37,10 @@
 #define ENGLISH
 
 #ifdef ENGLISH
-#define UNITS_IMPERIAL
+#define UNITS_METRIC
 #define CHAR_DEC_SEPARATOR CHAR_PERIOD // Period is used as a decimal separator only in the UK and the US.
 #else
-#define UNITS_METRIC
+#define UNITS_IMPERIAL
 #define CHAR_DEC_SEPARATOR CHAR_COMMA
 #endif
 
